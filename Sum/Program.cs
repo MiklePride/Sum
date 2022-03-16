a45e6922ab9b1eb33a1c5bae0154c8ebd5eb56a5
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-16 22:19:30 +0300

    re

--- Sum/Program.cs
+++ Sum/Program.cs
@@ -2,9 +2,11 @@
 {
     static void Main(string[] args)
     {
-        Random rand = new Random();
-        int number = rand.Next(0, 101);
-        int sum = 0;
+        Random random = new Random();
+        int minimumRandomValue = 0;
+        int maximumRandomValue = 101;
+        int number = random.Next(minimumRandomValue, maximumRandomValue);
+        int sumOfMultiples = 0;
         int multiplicityOfThree = 3;
         int multiplicityOfFive = 5;
        
@@ -12,11 +14,7 @@
         {
             if (i % multiplicityOfThree == 0 || i % multiplicityOfFive == 0)
             {
-                sum += i;
-            }
-            else
-            {
-                continue;
+                sumOfMultiples += i;
             }
         }
     }
