c5398d013bf6d8ba30ab3969ef9db3867a6c66af
Михаил Денисов <migelius.invesy@gmail.com>
2022-03-16 16:30:17 +0300

    Добавьте файлы проекта.

---
+++ Sum/Program.cs
@@ -0,0 +1,16 @@
+﻿class program
+{
+    static void Main(string[] args)
+    {
+        Random rand = new Random();
+        int number = rand.Next(0, 101);
+        int multiplicity = 5;
+        Console.WriteLine(number);
+        
+        for (int i = multiplicity; i < number; i += 5)
+        {
+            Console.WriteLine(i);
+        }
+        
+    }
+}
